76c9bf0a2c5039aff1a1aa072476a3d2fcd1250b
Francesco Zorzi <frensinik89@yahoo.it>
2023-08-29 19:05:34 +0200

    triple A pattern

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -2,11 +2,21 @@ import { it, expect } from "vitest";
 import { add, subtract } from "./math";
 
 it("should sum all number values in an array", () => {
-  const result = add([1, 2, 3, 4, 5]);
-  expect(result).toBe(15);
+  // Arrange
+  const numbers = [1, 2, 3, 4, 5];
+  const expected = numbers.reduce((a, b) => a + b, 0);
+  // Act
+  const result = add(numbers);
+  // Assert
+  expect(result).toBe(expected);
 });
 
 it("should subtract all number values in an array", () => {
-  const result = subtract([1, 2, 3, 4, 5]);
-  expect(result).toBe(-15);
+  // Arrange
+  const numbers = [1, 2, 3, 4, 5];
+  const expected = numbers.reduce((a, b) => a - b, 0);
+  // Act
+  const result = subtract(numbers);
+  // Assert
+  expect(result).toBe(expected);
 });
