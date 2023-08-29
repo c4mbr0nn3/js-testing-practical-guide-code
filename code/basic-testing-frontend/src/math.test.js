11ae3959a38486b5acbae2534bc016a54ff3369f
Francesco Zorzi <frensinik89@yahoo.it>
2023-08-29 19:23:01 +0200

    test for errors

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -12,30 +12,38 @@ it("add should sum all number values in an array", () => {
 });
 
 it("add should return NaN if any value is not a number", () => {
-  // Arrange
   const numbers = [1, 2, "a"];
-  // Act
   const result = add(numbers);
-  // Assert
   expect(result).toBeNaN();
 });
 
 it("add should return correct sum if a string array is provided", () => {
-  // Arrange
   const numbers = ["1", "2"];
   const expected = numbers.reduce((a, b) => +a + +b, 0);
-  // Act
   const result = add(numbers);
-  // Assert
   expect(result).toBe(expected);
 });
 
+it("add should return 0 if an empty array is provided", () => {
+  const numbers = [];
+  const expected = 0;
+  const result = add(numbers);
+  expect(result).toBe(expected);
+});
+
+it("add should throw an error if no array is provided", () => {
+  const resultFn = () => add();
+  expect(resultFn).toThrow();
+});
+
+it("add should throw an error if numbers not in an array", () => {
+  const resultFn = () => add(1, 2);
+  expect(resultFn).toThrow();
+});
+
 it("subtract should subtract all number values in an array", () => {
-  // Arrange
   const numbers = [1, 2]; // keep it simple, just two numbers
   const expected = numbers.reduce((a, b) => a - b, 0);
-  // Act
   const result = subtract(numbers);
-  // Assert
   expect(result).toBe(expected);
 });
