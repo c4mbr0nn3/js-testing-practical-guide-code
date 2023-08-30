ae47a58418611bbc3b1d68f5243eb8bceac351d0
Francesco Zorzi <francesco.zorzi@iasonltd.com>
2023-08-30 13:14:38 +0200

    add describe directive

--- code/basic-testing-frontend/src/util/validation.test.js
+++ code/basic-testing-frontend/src/util/validation.test.js
@@ -1,31 +1,35 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { validateStringNotEmpty, validateNumber } from "./validation";
 
-test("validateStringNotEmpty should throw an error if the input is empty", () => {
-  expect(() => validateStringNotEmpty("")).toThrow(
-    "Invalid input - must not be empty."
-  );
+describe("validateStringNotEmpty()", () => {
+  test("should throw an error if the input is empty", () => {
+    expect(() => validateStringNotEmpty("")).toThrow(
+      "Invalid input - must not be empty."
+    );
+  });
+
+  test("should throw an error if the input only contains spaces", () => {
+    expect(() => validateStringNotEmpty("   ")).toThrow(
+      "Invalid input - must not be empty."
+    );
+  });
+
+  test("should throw an error if the input is not a string", () => {
+    expect(() => validateStringNotEmpty(1)).toThrow();
+  });
+
+  test("should not throw an error if the input is not empty", () => {
+    expect(() => validateStringNotEmpty("a")).not.toThrow();
+  });
 });
 
-test("validateStringNotEmpty should throw an error if the input only contains spaces", () => {
-  expect(() => validateStringNotEmpty("   ")).toThrow(
-    "Invalid input - must not be empty."
-  );
-});
+describe("validateNumber()", () => {
+  test("should throw an error if the input is not a number", () => {
+    expect(() => validateNumber("a")).toThrow("Invalid number input.");
+    expect(() => validateNumber("1")).toThrow("Invalid number input.");
+  });
 
-test("validateStringNotEmpty should throw an error if the input is not a string", () => {
-  expect(() => validateStringNotEmpty(1)).toThrow();
-});
-
-test("validateStringNotEmpty should not throw an error if the input is not empty", () => {
-  expect(() => validateStringNotEmpty("a")).not.toThrow();
-});
-
-test("validateNumber should throw an error if the input is not a number", () => {
-  expect(() => validateNumber("a")).toThrow("Invalid number input.");
-  expect(() => validateNumber("1")).toThrow("Invalid number input.");
-});
-
-test("validateNumber should not throw an error if the input is a number", () => {
-  expect(() => validateNumber(1)).not.toThrow();
+  test("should not throw an error if the input is a number", () => {
+    expect(() => validateNumber(1)).not.toThrow();
+  });
 });
