d4c2c52b7b5c938afe91c489664ce2c431a8f7c5
Francesco Zorzi <francesco.zorzi@iasonltd.com>
2023-08-30 10:31:46 +0200

    switch to test instead of it

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,7 +1,7 @@
-import { it, expect } from "vitest";
+import { test, expect } from "vitest";
 import { add, subtract } from "./math";
 
-it("add should sum all number values in an array", () => {
+test("add should sum all number values in an array", () => {
   // Arrange
   const numbers = [1, 2]; // keep it simple, just two numbers
   const expected = numbers.reduce((a, b) => a + b, 0);
@@ -11,37 +11,37 @@ it("add should sum all number values in an array", () => {
   expect(result).toBe(expected);
 });
 
-it("add should return NaN if any value is not a number", () => {
+test("add should return NaN if any value is not a number", () => {
   const numbers = [1, 2, "a"];
   const result = add(numbers);
   expect(result).toBeNaN();
 });
 
-it("add should return correct sum if a string array is provided", () => {
+test("add should return correct sum if a string array is provided", () => {
   const numbers = ["1", "2"];
   const expected = numbers.reduce((a, b) => +a + +b, 0);
   const result = add(numbers);
   expect(result).toBe(expected);
 });
 
-it("add should return 0 if an empty array is provided", () => {
+test("add should return 0 if an empty array is provided", () => {
   const numbers = [];
   const expected = 0;
   const result = add(numbers);
   expect(result).toBe(expected);
 });
 
-it("add should throw an error if no array is provided", () => {
+test("add should throw an error if no array is provided", () => {
   const resultFn = () => add();
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/);
 });
 
-it("add should throw an error if numbers not in an array", () => {
+test("add should throw an error if numbers not in an array", () => {
   const resultFn = () => add(1, 2);
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/);
 });
 
-it("subtract should subtract all number values in an array", () => {
+test("subtract should subtract all number values in an array", () => {
   const numbers = [1, 2]; // keep it simple, just two numbers
   const expected = numbers.reduce((a, b) => a - b, 0);
   const result = subtract(numbers);
