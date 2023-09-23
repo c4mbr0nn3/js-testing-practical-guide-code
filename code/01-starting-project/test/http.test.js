1beeb41934643f6e8d0e79fbe0086d78c9827b0b
Francesco Zorzi <francesco.zorzi@iasonltd.com>
2023-09-23 21:52:56 +0200

    feat: add test for promise rejects

--- code/01-starting-project/test/http.test.js
+++ code/01-starting-project/test/http.test.js
@@ -5,6 +5,9 @@ const responseData = { testKey: "testValue" };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      reject(new Error("No stringified body provided!"));
+    }
     const response = {
       ok: true,
       json() {
@@ -22,5 +25,18 @@ describe("http utils", () => {
   it("should return any available response data", () => {
     const testData = { key: "data" };
     return expect(sendDataRequest(testData)).resolves.toEqual(responseData);
+  });
+
+  it("should convert the provided data to JSON before sending it", async () => {
+    const testData = { key: "data" };
+    let errorMessage;
+
+    try {
+      await sendDataRequest(testData);
+    } catch (error) {
+      errorMessage = error.message;
+    }
+
+    expect(errorMessage).not.toBe("No stringified body provided!");
   });
 });
