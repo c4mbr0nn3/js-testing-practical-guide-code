a4b138c04ab2a306d6e592eee39faf2aa41a52e0
Francesco Zorzi <francesco.zorzi@iasonltd.com>
2023-09-23 21:15:40 +0200

    feat: add tests from chatgpt

---
+++ code/01-starting-project/test/errors.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { HttpError, ValidationError } from "./../util/errors";
+
+describe("HttpError", () => {
+  it("should correctly assign properties", () => {
+    const error = new HttpError(404, "Not Found", {
+      detail: "Resource not found",
+    });
+
+    expect(error.statusCode).toBe(404);
+    expect(error.message).toBe("Not Found");
+    expect(error.data).toEqual({ detail: "Resource not found" });
+  });
+});
+
+describe("ValidationError", () => {
+  it("should correctly assign message property", () => {
+    const error = new ValidationError("Validation failed");
+
+    expect(error.message).toBe("Validation failed");
+  });
+});
